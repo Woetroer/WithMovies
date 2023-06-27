4500aeceb255f416ff138f6c92b44f1376d972cd
Ivo Dijkman <ivo.dijkman@itvitaelearning.nl>
2023-06-27 11:51:34 +0200

    Initial

---
+++ WithMovies/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace WithMovies
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

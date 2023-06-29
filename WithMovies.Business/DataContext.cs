16bf026c22a4e40ea1e7d050c71cb65cc75bfdbe
hugo4it <hugo.vandekuilen@itvitaelearning.nl>
2023-06-29 14:48:08 +0200

    Datacontext

--- WithMovies.Business/DataContext.cs
+++ WithMovies.Business/DataContext.cs
@@ -1,7 +1,21 @@
-﻿namespace WithMovies.Business
-{
-    public class DataContext
-    {
+﻿using Microsoft.EntityFrameworkCore;
+using WithMovies.Domain.Models;
 
+namespace WithMovies.Business
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<CastMember> CastMembers { get; set; } = null!;
+        public DbSet<Credits> Credits { get; set; } = null!;
+        public DbSet<CrewMember> CrewMembers { get; set; } = null!;
+        public DbSet<Keyword> Keywords { get; set; } = null!;
+        public DbSet<Movie> Movies { get; set; } = null!;
+        public DbSet<MovieCollection> MovieCollections { get; set; } = null!;
+        public DbSet<ProductionCompany> ProductionCompanies { get; set; } = null!;
+        public DbSet<RecommendationProfile> RecommendationProfiles { get; set; } = null!;
+        public DbSet<Review> Reviews { get; set; } = null!;
+
+        public DataContext() : base() { }
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
     }
 }
--- WithMovies.Domain/Models/Keyword.cs
+++ WithMovies.Domain/Models/Keyword.cs
@@ -8,7 +8,7 @@ namespace WithMovies.Domain.Models
 {
     public class Keyword : BaseEntity
     {
-        public required virtual ICollection<Movie> Movies { get; set; }
         public required string Name { get; set; }
+        public required virtual ICollection<Movie> Movies { get; set; }
     }
 }

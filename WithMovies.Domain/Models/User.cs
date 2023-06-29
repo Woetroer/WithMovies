f80e0146cd7e6341d35dc273fad8c16f248a26d3
Wout <wout3003@gmail.com>
2023-06-29 13:02:25 +0200

    User object

--- WithMovies.Domain/Models/User.cs
+++ WithMovies.Domain/Models/User.cs
@@ -1,16 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace WithMovies.Domain.Models
+﻿namespace WithMovies.Domain.Models
 {
     public class User : BaseEntity
     {
-        public required List<string> Friends { get; set; }
-        public required List<string> Watchlist { get; set; }
-        
+        public required ICollection<User> Friends { get; set; }
+        public required ICollection<Movie> Watchlist { get; set; }
     }
 }

7948c55ada673373c7476ce54d04319c54c2d734
Ivo Dijkman <ivo.dijkman@itvitaelearning.nl>
2023-06-29 11:51:58 +0200

    Made credits and keywords objects, started user object

---
+++ WithMovies.Domain/Models/Credits.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WithMovies.Domain.Models
+{
+    public class Credits : BaseEntity
+    {
+        public required int MovieId {get; set;}
+        public required string Cast { get; set;}
+        public required string Crew { get; set;}
+    }
+}

40fc3d7a8e636ffd4386ad9ae1bad03047b3b45d
Thijmen Aarts <thijmen.aarts@itvitaelearning.nl>
2023-06-29 12:07:43 +0200

    Added CrewMember and enums

---
+++ WithMovies.Domain/Enums/Department.cs
@@ -0,0 +1,16 @@
+﻿namespace WithMovies.Domain.Enums;
+
+public enum Department
+{
+    Lighting,
+    Camera,
+    Sound,
+    Production,
+    Writing,
+    CostumeAndMakeUp,
+    Directing,
+    Editing,
+    Crew,
+    VisualEffects,
+    Art,
+}
---
+++ WithMovies.Domain/Models/CrewMember.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WithMovies.Domain.Enums;
+
+namespace WithMovies.Domain.Models
+{
+    public class CrewMember
+    {
+        public required int MovieId { get; set; }
+        public required string CreditId { get; set; }
+        public required Department Department { get; set; }
+        public required int Gender { get; set; }
+        public required Job Job { get; set; }
+        public required string Name { get; set; }
+        public string? ProfilePath { get; set; }
+    }
+}

e507abf48e5203d615d7ebe86e810327164e204f
Ivo Dijkman <ivo.dijkman@itvitaelearning.nl>
2023-06-30 14:54:55 +0200

    Start of review adding

---
+++ WithMovies.WebApi/Controllers/ReviewController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WithMovies.Domain.Interfaces;
+
+namespace WithMovies.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReviewController : ControllerBase
+    {
+        private readonly IReviewService _reviewService;
+        public ReviewController(IReviewService reviewService)
+        {
+            _reviewService = reviewService;
+        }
+
+        public record ReviewToAdd(int Userid, int MovieId, int Rating, string? Message, DateTime PosedTime);
+        [HttpPost]
+        public async Task<IActionResult>CreateReview(ReviewToAdd reviewToAdd)
+        {
+
+        }
+    }
+}

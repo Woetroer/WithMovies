d4ddef800a3d10dc373b88a2bca38ebf64dfd987
Ivo Dijkman <ivo.dijkman@itvitaelearning.nl>
2023-06-30 14:54:55 +0200

    Start of review adding

---
+++ WithMovies.Business/Services/ReviewService.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WithMovies.Domain.Models;
+using WithMovies.Domain.Interfaces;
+
+namespace WithMovies.Business.Services
+{
+    public class ReviewService : IReviewService
+    {
+        private DataContext _dataContext;
+        private ILogger<MovieService> _logger;
+
+        public async Task Create(int userId, int movieId, int rating, string? message, DateTime postedTime)
+        {
+            Review reviewToAdd = new Review()
+            {
+                UserId = userId,
+                MovieId = movieId,
+                Rating = rating,
+                Message = message,
+                PostedTime = postedTime
+            };
+            await _dataContext.Reviews.AddAsync(reviewToAdd);
+            await _dataContext.SaveChangesAsync();
+        }
+    }
+}
---
+++ WithMovies.Domain/Interfaces/IReviewService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WithMovies.Domain.Interfaces
+{
+    public interface IReviewService
+    {
+        Task Create(int userId, int movieId, int rating, string? message, DateTime postedTime);
+    }
+}

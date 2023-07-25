2d911f30d1155c7690e9d70737cb241fa58438ef
Wout <wout3003@gmail.com>
2023-07-25 14:39:24 +0200

    Merge

--- WithMovies.Domain/Models/User.cs
+++ WithMovies.Domain/Models/User.cs
@@ -7,9 +7,6 @@
         public required virtual ICollection<Review> Reviews { get; set; }
         public string RefreshToken { get; set; } = string.Empty;
         public DateTime RefreshTokenExpiry { get; set; }
-        public virtual ICollection<User> Friends { get; set; } = null!;
-        public virtual ICollection<Movie> Watchlist { get; set; } = null!;
-        public virtual ICollection<Review> Reviews { get; set; } = null!;
         public virtual RecommendationProfile RecommendationProfile { get; set; } = null!;
         public DateTime LastLogin { get; set; } = DateTime.Now;
     }

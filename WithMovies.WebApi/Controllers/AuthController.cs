4c77e3fbe9bce5355667efd94585af0cda3e4f24
Wout <wout3003@gmail.com>
2023-07-27 11:14:39 +0200

    .

--- WithMovies.WebApi/Controllers/AuthController.cs
+++ WithMovies.WebApi/Controllers/AuthController.cs
@@ -1,3 +1,15 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+using WithMovies.Business;
+using WithMovies.Domain.Models;
+using WithMovies.WebApi.Models;
+
 namespace WithMovies.WebApi.Controllers
 {
     [Route("api/[controller]")]
